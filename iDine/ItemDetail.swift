955ace8a5c322170322e0a9a1ca11d12897e56ec
Aiden <aidenbak9@gmail.com>
2025-09-03 15:10:03 -0500

    added orderview

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ItemDetail : View {
+    @EnvironmentObject var order: Order
     let item: MenuItem
 
     var body: some View {
@@ -25,6 +26,10 @@ struct ItemDetail : View {
             }
             Text(item.description)
                 .padding()
+            Button("Order This") {
+                order.add(item: item)
+            }
+            .buttonStyle(.borderedProminent)
             Spacer()
         }
         .navigationTitle(item.name)
@@ -34,5 +39,7 @@ struct ItemDetail : View {
 }
 
 #Preview {
-    ItemDetail(item: MenuItem.example)
+    NavigationStack {
+        ItemDetail(item: MenuItem.example).environmentObject(Order())
+    }
 }
