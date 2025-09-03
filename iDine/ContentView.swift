2ad304fcbdacf9aaa81b319e5a4c95c8d94495dc
Aiden <aidenbak9@gmail.com>
2025-09-03 10:31:37 -0500

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Aiden Baker on 8/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(.grouped)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,21 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Aiden Baker on 8/29/25.
+//
+
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    @StateObject var order = Order()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(order)
+        }
+    }
+}
