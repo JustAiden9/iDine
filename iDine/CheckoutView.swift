25f9c70d1d70216a01d97df5e5df53710e3aecdc
Aiden <aidenbak9@gmail.com>
2025-09-03 15:20:13 -0500

    created checkoutview, with form for cash, iDine points, and credit

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,53 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Aiden Baker on 9/3/25.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var paymentType = "Cash"
+    @State private var tipAmount = 15
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    let tipAmounts = [10, 15, 20, 25, 0]
+    var body: some View {
+        VStack {
+            Form {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+                Section("Add a tip?") {
+                    Picker("Percentage:", selection: $tipAmount) {
+                        ForEach(tipAmounts, id: \.self) {
+                            Text("\($0)%")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Total: $100") {
+                    Button("Confirm order") {
+                        // place the order
+                    }
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    CheckoutView().environmentObject(Order())
+}
--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -21,6 +21,7 @@ struct ItemRow : View {
                     .font(.headline)
                 Text("$\(item.price)")
             }
+            Spacer()
             ForEach(item.restrictions, id: \.self) { restriction in
                 Text(restriction)
                     .font(.caption)
